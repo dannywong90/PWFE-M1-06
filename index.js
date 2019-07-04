434d743f2a9d685e95b28d04552e71c0d124bb20
dannywong90 <riotfeme@gmail.com>
2019-07-03 21:45:04 -0300

    Especificando el puerto para heroku y local

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
+//Aca empieza un basico createServer
 const http = require("http");
 const fs = require("fs");
-
+const port = process.env.PORT || 80/*EN TODA INSTRUCION TIENE QUE TENER PORT*/ 
 http.createServer((request, response) => {
-		
-		let dir = "public/";
+        
+        let dir = "public/"; //declaro la carpeta en donde debe ir a buscar archivos
 
-		let file = (request.url == "/") ? "index.html" : request.url;
-			file = (file.match(/[^.]+(\.[^?#]+)?/) || [])[0];
-		
-		let ext = file.substring( file.lastIndexOf(".") ).toLowerCase();
+        let file = (request.url == "/") ? "index.html" : request.url;
+            file = (file.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+        
+        let ext = file.substring( file.lastIndexOf(".") ).toLowerCase();
 
-		let types = {
-			".html"	: "text/html",
-			".js"	: "text/javascript",
-			".css"	: "text/css",
-			".txt" 	: "text/plain",
-			".json"	: "application/json",
-			".png"	: "image/png",
-			".jpg"	: "image/jpg",
-			".gif"	: "image/gif",
-			".ico"	: "image/x-icon",
-			".wav"	: "audio/wav",
-			".mp4"	: "video/mp4",
-			".woff"	: "application/font-woff",
-			".ttf"	: "application/font-ttf",
-			".eot"	: "application/vnd.ms-fontobject",
-			".otf"	: "application/font-otf",
-			".svg"	: "application/image/svg+xml"
-		};
+        let types = {
+            ".html"    : "text/html",
+            ".js"    : "text/javascript",
+            ".css"    : "text/css",
+            ".txt"     : "text/plain",
+            ".json"    : "application/json",
+            ".png"    : "image/png",
+            ".jpg"    : "image/jpg",
+            ".gif"    : "image/gif",
+            ".ico"    : "image/x-icon",
+            ".wav"    : "audio/wav",
+            ".mp4"    : "video/mp4",
+            ".woff"    : "application/font-woff",
+            ".ttf"    : "application/font-ttf",
+            ".eot"    : "application/vnd.ms-fontobject",
+            ".otf"    : "application/font-otf",
+            ".svg"    : "application/image/svg+xml"
+        };
 
-		let contentType = types[ext] || "application/octet-stream";
+        let contentType = types[ext] || "application/octet-stream";
 
-		fs.readFile( dir + file, (error, content) => {
-			
-			if ( error ) {
-				response.writeHead(400, { "Content-Type" : "text/plain" } );
-				response.end("ARCHIVO NO ENCONTRADO");
-			} else {
-				response.writeHead(200, { "Content-Type" : contentType } );
-				response.end(content);
-			}
+        fs.readFile( dir + file, (error, content) => {
+            console.log("El recurso solicitado es: " + file)
+            if ( error ) {
+                response.writeHead(400, { "Content-Type" : "text/plain" } );
+                response.end("ARCHIVO NO ENCONTRADO");
+            } else {
+                response.writeHead(200, { "Content-Type" : contentType } );
+                response.end(content);
+            }
 
-		});
+        });
 
-}).listen(4000);
+}).listen(port);
